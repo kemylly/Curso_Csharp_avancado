9c39239eea3ae73dcbff4b990a4fb036134c73a0
kemylly <kemylly@users.noreply.github.com>
2020-09-03 23:44:25 -0300

    Adicionei cadastro de usuario e MDIparent1

--- Cadastro_usuario/Loja/Program.cs
+++ Cadastro_usuario/Loja/Program.cs
@@ -8,8 +8,16 @@ namespace Loja
 {
     class Program
     {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        //[STAThread]
+
         static void Main(string[] args)
         {
+           // Application.EnableVisualStyles();
+           // Application.SetCompatibleTextRenderingDefault(false);
+           // Application.Run(new MDIParent1());
         }
     }
 }
