aca6fb8520b56a5ea62ead644c8b77edb6ecd287
kemylly <kemylly@users.noreply.github.com>
2020-09-04 14:38:14 -0300

    Adicionei o Banco de dados

---
+++ Loja.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE Loja;
+GO
+USE Loja;
+
+CREATE TABLE tb_usarios(
+cod_usuario INT IDENTITY,
+nome VARCHAR (50) NOT NULL,
+login_usu VARCHAR (50) NOT NULL, 
+email VARCHAR (50) NOT NULL, 
+senha VARCHAR(30) NOT NULL, 
+cadastro TIME NOT NULL,
+situacao VARCHAR (50) NOT NULL, 
+perfil INT NOT NULL
+);
+
+
+
